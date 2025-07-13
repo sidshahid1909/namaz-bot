ee9f2832f0a6795c5c085bbb26c13b15d6b5dc9b
sidshahid1909 <shahidansari1909@gmail.com>
2025-07-13 18:02:12 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Google Sheets setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("/etc/secrets/credentials.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("Namaz time table").worksheet("Sheet1")
 rows = sheet.get_all_values()
